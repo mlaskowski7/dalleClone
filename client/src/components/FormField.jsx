f603cde76af01db3002b8bdbb0305102e37cdc32
Mateusz Laskowski <mlaskowski7>
2024-01-01 01:48:23 +0100

    finished create post page front end, now starting back end work

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
-const FormField = ({ LabelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe}) => {
+const FormField = ({ labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe}) => {
   return (
     <div>
       <div className="flex items-center gap-2 mb-2">
         <label htmlFor={name} className="block text-sm font-medium text-gray-900">
-          {LabelName}
+          {labelName}
         </label>
         {isSurpriseMe && (
-          <button type='button' onClick={handleSurpriseMe} className='font-semiblod text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'>
+          <button type='button' onClick={handleSurpriseMe} className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'>
             Surprise me
           </button>
         )}
       </div>
-      <input type={type}
+      <input 
+        type={type}
         id={name}
         name={name}
         placeholder={placeholder}
@@ -26,4 +27,4 @@ const FormField = ({ LabelName, type, name, placeholder, value, handleChange, is
   )
 }
 
-export default FormField
+export default FormField;
